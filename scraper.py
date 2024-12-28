fd68b37c157313c347e7307a96295c4332502c24
Harrison Cho <hcho1265@gmail.com>
2024-12-28 14:41:30 -0500

    script for json files breaking around line 53

--- scraper.py
+++ scraper.py
@@ -1,42 +1,138 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
-def DownloadLedger(url: str, downloads_path: str, chromedriver_path: str):
-    """
-    Automatically download CSV with logs, names by providing a csv URL
+import time 
+import random
 
-    INPUTS:
-    ------
 
-    url - (str) url for ledger you wish to download
-    downloads_path - (str) path to send downloaded csv files
-    chromedriver_path - (str) path where webdriver for google chrome is located.
-    """
-    # initialize path for downloads and chrome driver
+def WebCrawler(profile_path, profile_name, downloads_path):
+
     options = webdriver.ChromeOptions()
-    prefs = {"download.default_directory": downloads_path}
-    options.add_experimental_option("prefs", prefs)
-    driver = webdriver.Chrome(executable_path=chromedriver_path, options=options)
-    driver.get(url)
+    options.add_argument(f"--user-data-dir = {profile_path}")    
+    options.add_argument(f"--profile-directory= {profile_name}")
+    options.add_experimental_option('detach', True)
+
+    prefs = {"download.default_directory" : f"{downloads_path}"}
+    options.add_experimental_option("prefs",prefs)
+    
+    #service = Service(executable_path=chromedriver_path)
+    driver = webdriver.Chrome(options=options)
+
+    driver.get('https://plus.pokernow.club/')
     driver.maximize_window()
 
-    # specified XPath for buttons of interest
-    bottom_button_XPATH = "/html/body/div/div/div[1]/div[3]/div[6]"
-    ledger_button_XPATH = (
-        "/html/body/div[1]/div/div[1]/div[3]/div[2]/div/div[2]/div[2]/button[3]"
-    )
-    download_ledger_XPATH = (
-        "/html/body/div/div/div[1]/div[3]/div[2]/div/div[2]/div[2]/div"
-    )
 
-    # click first button for showing modal overlay
-    bottom_button = driver.find_element(By.XPATH, bottom_button_XPATH)
-    bottom_button.click()
+    login = driver.find_element(By.XPATH, '//*[@id="content"]/a')
+    login.click()
 
-    # click ledger button in modal overlay
-    ledger_button = bottom_button.find_element(By.XPATH, ledger_button_XPATH)
-    ledger_button.click()
+    #input username 
+    inputbox = driver.find_element(By.XPATH, '//*[@id="user_login_form_email"]')
+    inputbox.send_keys('hcho1265@gmail.com')
+    
+    #click button 
+    login_button = driver.find_element(By.XPATH, '//*[@id="new_user_login_form"]/input[3]')
+    login_button.click()
 
-    # download the ledger csv
-    download_ledger = ledger_button.find_element(By.XPATH, download_ledger_XPATH)
+    #login with code 
+    code_to_input = input('Enter Code From Email:')
+    confirmation_code_box = driver.find_element(By.XPATH, '//*[@id="user_login_code_form_code"]')
+    confirmation_code_box.send_keys(code_to_input)
+    login_button2 = driver.find_element(By.XPATH, '//*[@id="new_user_login_code_form"]/input[3]')
+    login_button2.click()
+
+
+    # once logged in, scrape all json files 
+
+    #table = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '(//table)[1]'))).get_attribute("outerHTML")
+    #game_table = WebDriverWait(driver, 20).until(EC.vivisibility_of_element_located((By.XPATH, '//*[@id="myGames"]/tbody')))
+
+    game_table = driver.find_elements(By.XPATH, '//*[@id="myGames"]/tbody')
+
+    for i in range(1, len(game_table) + 1): 
+        game_table[i].click()
+        time.sleep(1)
+        download_button = driver.find_element(By.XPATH, '//*[@id="rowJson"]/td[2]/a')
+        download_button.click()
+        back_button = driver.find_element(By.XPATH, '//*[@id="pagetitle"]/div/div[1]/i')
+        back_button.click()
+        
+
+# specify paths 
+WebCrawler(profile_path, profile_name, downloads_path)
+
+
+
+
+
+
+# def DownloadLedger(url: str, downloads_path: str, chromedriver_path: str):
+#     """
+#     Automatically download CSV with logs, names by providing a csv URL
+
+#     INPUTS:
+#     ------
+
+#     url - (str) url for ledger you wish to download
+#     downloads_path - (str) path to send downloaded csv files
+#     chromedriver_path - (str) path where webdriver for google chrome is located.
+#     """
+#     # initialize path for downloads and chrome driver
+#     options = webdriver.ChromeOptions()
+#     prefs = {"download.default_directory": downloads_path}
+#     options.add_experimental_option("prefs", prefs)
+#     driver = webdriver.Chrome(executable_path=chromedriver_path, options=options)
+#     driver.get(url)
+#     driver.maximize_window()
+
+#     # specified XPath for buttons of interest
+#     bottom_button_XPATH = "/html/body/div/div/div[1]/div[3]/div[6]"
+#     ledger_button_XPATH = (
+#         "/html/body/div[1]/div/div[1]/div[3]/div[2]/div/div[2]/div[2]/button[3]"
+#     )
+#     download_ledger_XPATH = (
+#         "/html/body/div/div/div[1]/div[3]/div[2]/div/div[2]/div[2]/div"
+#     )
+
+#     # click first button for showing modal overlay
+#     bottom_button = driver.find_element(By.XPATH, bottom_button_XPATH)
+#     bottom_button.click()
+
+#     # click ledger button in modal overlay
+#     ledger_button = bottom_button.find_element(By.XPATH, ledger_button_XPATH)
+#     ledger_button.click()
+    
+
+#     # download the ledger csv
+#     download_ledger = ledger_button.find_element(By.XPATH, download_ledger_XPATH)
+#     time.sleep(random.randint(3,8))
+#     driver.close()
+
+
+
+
+# # step 1: log in 
+# # step 2: nagivate to https://plus.pokernow.club 
+# # step 3: select path 
+# def scrape_json():  
+#     driver = webdriver.Chrome()
+        
+#     # select table 
+#     games = driver.find_elements_by_xpath('//*[@id="myGames"]/tbody')
+#     for i in range(1, len(games) + 1): 
+
+#         games[i].click() 
+#         time.sleep(random.randint(1,8))
+
+        
+
+#     # step 3.1 - click //*[@id="myGames"]/tbody/tr[1]
+#     # step 3.2 - click download 
+#     # step 3.3 - click back to table and go to next element 
+
+    
+    
+    
